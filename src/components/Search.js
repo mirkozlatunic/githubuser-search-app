9fb7e930f1e76a430d17509803ca36451bda2c87
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-06-18 20:36:02 -0700

    search input bar styling completed

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,14 +2,14 @@ import searchIcon from "../assets/icon-search.svg";
 
 function Search() {
   return (
-    <div className="">
-      <img src={searchIcon} alt="search" className="absolute py-6 pl-8" />
+    <div className="relative flex w-full max-w-2/4">
+      <img src={searchIcon} alt="search" className="absolute py-6 pl-8 z-10" />
       <input
         type="text"
         placeholder="Search GitHub username..."
-        className="py-6 pl-20 rounded-2xl w-full"
+        className="py-6 pl-20 rounded-2xl w-full drop-shadow-xl dark:bg-dark-darkblue dark:drop-shadow-none dark:text-dark-white outline-none"
       ></input>
-      <button className="cursor-pointer bg-light-blue text-light-white px-6 py-3 rounded-xl absolute">
+      <button className="cursor-pointer bg-light-blue text-light-white px-6 py-3 rounded-xl absolute right-2 top-1/2 transform -translate-y-1/2">
         Search
       </button>
     </div>
